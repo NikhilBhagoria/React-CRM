5e8472bdbfcd522545d52bd9135319d537a171a7
NikhilBhagoria <123961691+NikhilBhagoria@users.noreply.github.com>
2024-04-14 18:17:17 +0530

    added Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import { useState } from 'react';
 import './App.css'
+import SideBar, { SidebarItem } from './components/SideBar/SideBar';
+import { LayoutDashboard, Home, StickyNote, Layers, Flag, Calendar, LifeBuoy, Settings } from "lucide-react";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <div className="flex">
+        <SideBar>
+          <SidebarItem icon={<Home size={20} />} text="Home" />
+          <SidebarItem icon={<LayoutDashboard size={20} />} text="Dashboard" active alert />
+          <SidebarItem icon={<StickyNote size={20} />} text="Projects" />
+          <SidebarItem icon={<Calendar size={20} />} text="Calendar" />
+          <SidebarItem icon={<Layers size={20} />} text="Tasks" />
+          <SidebarItem icon={<Flag size={20} />} text="Reporting" />
+          <hr className="my-3" />
+          <SidebarItem icon={<Settings size={20} />} text="Settings" />
+          <SidebarItem icon={<LifeBuoy size={20} />} text="Help" />
+        </SideBar>
+      </div>
     </>
   )
 }
