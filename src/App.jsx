68d51cd8bed288ec8b05ca6f56b31e006ef611a2
Mohit <mohitkhatri4272@gmail.com>
2024-10-27 12:46:31 +0530

    added husky for the pre commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import { useState } from 'react'
 import './App.css'
-import SideBar, { SidebarItem } from './components/SideBar/SideBar'
-import { LayoutDashboard, Home, StickyNote, Layers, Flag, Calendar, LifeBuoy, Settings } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
 
 function App() {
